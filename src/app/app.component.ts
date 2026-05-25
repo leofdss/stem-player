ec9807cdc6ea3046321b4547f3fd48ef537ba5e3
Leonardo Farias de Souza Silva <leonardo.f.desouzasilva@gmail.com>
2026-05-25 16:58:29 -0300

    refactor: use angular signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { invoke } from "@tauri-apps/api/core";
 
@@ -9,14 +9,14 @@ import { invoke } from "@tauri-apps/api/core";
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
-  greetingMessage = "";
+  greetingMessage = signal('');
 
   greet(event: SubmitEvent, name: string): void {
     event.preventDefault();
 
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
+      this.greetingMessage.set(text);
     });
   }
 }
