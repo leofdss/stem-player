a498b945a55363465ca8b4cc9da4716b55799c42
Leonardo Farias de Souza Silva <leonardo.f.desouzasilva@gmail.com>
2026-05-25 15:53:13 -0300

    init openspec

--- post-install.sh
+++ post-install.sh
@@ -3,3 +3,6 @@
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 code --install-extension rust-lang.rust-analyzer --install-extension vadimcn.vscode-lldb
+
+npm install -g @fission-ai/openspec@latest
+npm install -g @anthropic-ai/claude-code
